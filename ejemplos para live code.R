8fb5b11bb0e300ebad6e3ce90aff86e5a85ccc16
BetsyCohen <betsabecohen@gmail.com>
2025-05-07 19:04:55 -0300

    Actualizo lo de waiter y otros

--- ejemplos para live code.R
+++ ejemplos para live code.R
@@ -148,4 +148,197 @@
 # 
 # shinyApp(ui, server)
 
+## Ejemplo de notificaciones progresivas
 
+# library(shiny)
+# 
+# ui <- fluidPage(
+#   actionButton("start", "Iniciar carga de datos"),
+#   tableOutput("data")
+# )
+# 
+# server <- function(input, output, session) {
+#   notify <- function(msg, id = NULL) {
+#     showNotification(msg, id = id, duration = NULL, closeButton = FALSE)
+#   }
+#   
+#   data <- eventReactive(input$start, { 
+#     id <- notify("Leyendo datos...")
+#     on.exit(removeNotification(id), add = TRUE)
+#     Sys.sleep(1)
+#     
+#     notify("Reticulando splines...", id = id)
+#     Sys.sleep(1)
+#     
+#     notify("Agrupando llamas...", id = id)
+#     Sys.sleep(1)
+#     
+#     notify("Ortogonalizando matrices...", id = id)
+#     Sys.sleep(1)
+#     
+#     mtcars  # dataset incorporado en R
+#   })
+#   
+#   output$data <- renderTable(head(data()))
+# }
+# 
+# shinyApp(ui, server)
+
+
+
+
+# ## Ejemplo de barras de progreso con waiterss
+# 
+# 
+# library(shiny)
+# library(waiter)
+# 
+# ui <- navbarPage("Waitress aplicado al nav",
+#                  tabPanel("home",
+#                           useWaitress(color = "#7F7FFF"),
+#                           actionButton("go", "Generar gráfico"),
+#                           plotOutput("plot")
+#                  )
+# )
+# 
+# server <- function(input, output, session) {
+#   
+#   # Creamos el objeto waitress para despues llamar sus metodos $start() $inic()
+#   # (creamos una sola vez la instancia y después lo volvemos a usar en el observeEvent)
+#   waitress <- Waitress$new(
+#     selector = "nav", 
+#     theme = "overlay-percent", 
+#     min = 0, 
+#     max = 10
+#   )
+#   
+#   observeEvent(input$go, {
+#     
+#     waitress$start()  # Muestra el loader
+#     
+#     output$plot <- renderPlot({
+#       for (i in 1:10) {
+#         waitress$inc(1)  # Incrementa 10%
+#         Sys.sleep(0.5)
+#       }
+#       waitress$close()  # Oculta el loader cuando termina
+#       hist(runif(100))
+#     })
+#     
+#   })
+# }
+# 
+# shinyApp(ui, server)
+
+
+## Spinner con shinycssloaders
+
+# library(shinycssloaders)
+# 
+# ui <- fluidPage(
+#   actionButton("go", "go"),
+#   withSpinner(plotOutput("plot")),
+# )
+# server <- function(input, output, session) {
+#   data <- eventReactive(input$go, {
+#     Sys.sleep(3)
+#     data.frame(x = runif(50), y = runif(50))
+#   })
+#   
+#   output$plot <- renderPlot(plot(data()), res = 96)
+# }
+# 
+# shinyApp(ui, server)
+
+
+
+## Confirmacion explicita
+
+# Definimos el modalDialog
+
+# modal_confirm <- modalDialog(
+#   "Are you sure you want to continue?",
+#   title = "Deleting files",
+#   footer = tagList(
+#     actionButton("cancel", "Cancel"),
+#     actionButton("ok", "Delete", class = "btn btn-danger")
+#   )
+# )
+# 
+# 
+# ui <- fluidPage(
+#   actionButton("delete", "Delete all files?")
+# )
+# 
+# 
+# server <- function(input, output, session) {
+#   observeEvent(input$delete, {
+#     ## en vez de desatar el evento borrar corre el dialogo
+#     showModal(modal_confirm)
+#   })
+#   
+#   observeEvent(input$ok, {
+#     showNotification("Files deleted")
+#     removeModal()
+#   })
+#   observeEvent(input$cancel, {
+#     removeModal()
+#   })
+# }
+# 
+# shinyApp(ui, server)
+
+# ## Deshacer accion -----------
+# 
+# ui <- fluidPage(
+#   textAreaInput("message", 
+#                 label = NULL, 
+#                 placeholder = "What's happening?",
+#                 rows = 3
+#   ),
+#   actionButton("tweet", "Tweet")
+# )
+# 
+# runLater <- function(action, seconds = 3) {
+#   observeEvent(
+#     invalidateLater(seconds * 1000), action, 
+#     ignoreInit = TRUE, 
+#     once = TRUE, 
+#     ignoreNULL = FALSE,
+#     autoDestroy = FALSE
+#   )
+# }
+# 
+# server <- function(input, output, session) {
+#   waiting <- NULL
+#   last_message <- NULL
+#   
+#   observeEvent(input$tweet, {
+#     notification <- glue::glue("Tweeted '{input$message}'")
+#     last_message <<- input$message
+#     updateTextAreaInput(session, "message", value = "")
+#     
+#     showNotification(
+#       notification,
+#       action = actionButton("undo", "Undo?"),
+#       duration = NULL,
+#       closeButton = FALSE,
+#       id = "tweeted",
+#       type = "warning"
+#     )
+#     
+#     waiting <<- runLater({
+#       cat("Actually sending tweet...\n")
+#       removeNotification("tweeted")
+#     })
+#   })
+#   
+#   observeEvent(input$undo, {
+#     waiting$destroy()
+#     showNotification("Tweet retracted", id = "tweeted")
+#     updateTextAreaInput(session, "message", value = last_message)
+#   })
+# }
+# 
+# 
+# shinyApp(ui, server)
